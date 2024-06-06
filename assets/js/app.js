ce1b019da6a5abf38995b22389eb1c0921fa4296
MeluCRT <acerato@mf.me>
2024-06-06 04:10:13 +0200

    kitty JS + assets

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,52 @@
 "use strict";
 
-// FADEOUT
+/* playroom - love */
 
-document.querySelectorAll('a:not([href^="#"])').forEach(el => {
-    const main = document.querySelector("body");
-    if (!main) return;
-    el.addEventListener("click", (e) => {
-        if (!el.href || el.href[0] === "#") return;
-        e.preventDefault();
-        setTimeout(() => {
-            window.location = el.href;
-        }, 200);
-        main.classList.add("fadeout");
+function createHeart(x, y) {
+    var heart = document.createElement('div');
+    heart.innerText = '❤️';
+    heart.classList.add('heart');
+    document.body.appendChild(heart);
+
+    // ajout coeur puis calcul de position
+    const heartRect = heart.getBoundingClientRect();
+    heart.style.left = (x - heartRect.width / 2) + 'px';
+    heart.style.top = (y - heartRect.height / 2) + 'px';
+
+    setTimeout(() => {
+        heart.remove();
+    }, 1000);
+}
+
+/* playroom - random kitty */
+
+let cat = document.querySelector(".section__play__kitty");
+let purrSound = document.querySelector(".audio--purr");
+
+if (cat && purrSound) {
+
+    cat.addEventListener('click', function (e) {
+        // random kitty
+        var catRandom = getRandomIntInclusive(1, 4);
+        cat.setAttribute("src", "assets/images/kitty/cat_" + catRandom + ".png");
+
+        // ronron
+        purrSound.play();
+
+        // calcul position clic
+        var rect = cat.getBoundingClientRect();
+        var x = e.clientX - rect.left + rect.width / 2;
+        var y = e.clientY - rect.top + rect.height / 2;
+
+        // love
+        createHeart(x, y);
     });
-});
+}
+
+/* make it RANDOM */
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
