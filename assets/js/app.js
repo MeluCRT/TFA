e6a4d9b7f86f4ac2e0c7ec711b9ed65f6d40967b
MeluCRT <acerato@mf.me>
2024-06-11 12:55:51 +0200

    var/const -> let + mobile fix?

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,13 @@
 /* playroom - love */
 
 function createHeart(x, y) {
-    var heart = document.createElement('div');
+    let heart = document.createElement('div');
     heart.innerText = '❤️';
     heart.classList.add('heart');
     document.body.appendChild(heart);
 
     // ajout coeur puis calcul position
-    const heartRect = heart.getBoundingClientRect();
+    let heartRect = heart.getBoundingClientRect();
     heart.style.left = (x - heartRect.width / 2) + 'px';
     heart.style.top = (y - heartRect.height / 2) + 'px';
 
@@ -24,20 +24,15 @@ let cat = document.querySelector(".section__play__kitty");
 let purrSound = document.querySelector(".audio--purr");
 
 if (cat && purrSound) {
-    cat.addEventListener('click', function (e) {
-        handleCatClick(e);
-    });
-
-    cat.addEventListener('touchend', function (e) {
-        handleCatClick(e);
-    });
+    cat.addEventListener('click', handleCatClick);
+    cat.addEventListener('touchend', handleCatClick);
 }
 
 function handleCatClick(e) {
     e.preventDefault();
 
     // random kitty
-    var catRandom = getRandomIntInclusive(1, 9);
+    let catRandom = getRandomIntInclusive(1, 9);
     cat.setAttribute("src", "assets/images/kitty/cat_" + catRandom + ".png");
 
     // ronron
@@ -45,7 +40,7 @@ function handleCatClick(e) {
 
     // calcul position clic
     let x, y;
-    if (e.changedTouches) {
+    if (e.changedTouches && e.changedTouches.length > 0) {
         // toucher
         x = e.changedTouches[0].clientX + window.scrollX;
         y = e.changedTouches[0].clientY + window.scrollY;
